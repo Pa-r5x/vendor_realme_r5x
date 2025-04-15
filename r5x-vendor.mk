cb9fe293b46b0d4a65bf17d0f46c0a266a941c27
Amrito Karmokar <amritokarmokar5i@gmail.com>
2025-04-15 05:56:06 +0000

    r5x: sync with device tree
    Signed-off-by: Amrito Karmokar <amritokarmokar5i@gmail.com>

--- r5x-vendor.mk
+++ r5x-vendor.mk
@@ -383,20 +383,15 @@ PRODUCT_PACKAGES += \
     camera.trinket \
     vendor.oppo.hardware.camera.oppoHalAssist@1.0-impl \
     imx386_PdafLibrary \
-    libAlacSwDec \
-    libApeSwDec \
     libDxHdcp \
     libFileMux_proprietary \
-    libFlacSwDec \
     libOPPORectify \
     libOccluDetect \
     libOmxAacDec \
     libOmxAlacDec \
-    libOmxAlacDecSw \
     libOmxAmrDec \
     libOmxAmrwbplusDec \
     libOmxApeDec \
-    libOmxApeDecSw \
     libOmxDsdDec \
     libOmxEvrcDec \
     libOmxG711Dec \
@@ -1823,7 +1818,6 @@ PRODUCT_PACKAGES += \
     libdepthmap \
     libdepthmapwrapper \
     libdrc \
-    libdsd2pcm \
     libflash_pmic \
     libgcs-calwrapper \
     libgcs-ipc \
@@ -2044,9 +2038,11 @@ PRODUCT_PACKAGES += \
     libsurround_3mic_proc \
     libswresample \
     libswscale \
+    libswvdec \
     libsymphony-cpu \
     libsymphonypower \
     libthread_blur \
+    libvideoutils \
     libwfdcodecv4l2_proprietary \
     libwfdcommonutils_proprietary \
     libwfdconfigutils_proprietary \
@@ -2258,6 +2254,9 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.radio.uim_remote_client@1.2 \
     vendor.qti.hardware.radio.uim_remote_server@1.0 \
     vendor.qti.hardware.tui_comm@1.0 \
+    vendor.qti.hardware.vpp@1.1 \
+    vendor.qti.hardware.vpp@1.2 \
+    vendor.qti.hardware.vpp@1.3 \
     vendor.qti.ims.callinfo@1.0_vendor \
     vendor.qti.ims.rcsconfig@1.0_vendor \
     vendor.qti.ims.rcsconfig@1.1_vendor \
